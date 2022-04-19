13509c59b541b3cc76ff08320be558c121784bc8
565353780 <565353780@qq.com>
2022-04-19 22:59:10 +0800

    [RUN] finish simple install bash

--- README.md
+++ README.md
@@ -27,8 +27,16 @@ ln -s <path-to-Data-folder> ./Data
 
 ## Install
 
+### Simple
+
+```bash
+source setup.sh
 ```
-conda create -n roca python=3.8
+
+### Step by step
+
+```bash
+conda create -n roca python=3.8 -y
 conda activate roca
 
 git clone --recursive https://github.com/cangumeli/Scan2CADRasterizer.git
@@ -36,8 +44,18 @@ cd Scan2CADRasterizer
 pip install .
 cd ..
 
-cd image-to-cad
-source setup.sh
+pip install dataclasses opencv-python numpy-quaternion \
+  pandas scipy trimesh rtree numba open3d==0.13.0
+
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
+conda install -c bottler nvidiacub -y
+conda install pytorch3d -c pytorch3d-nightly -y
+
+pip install torch==1.7.0+cu110 torchvision==0.8.1+cu110 torchaudio==0.7.0 -f \
+  https://download.pytorch.org/whl/torch_stable.html
+
+python -m pip install detectron2==0.3+cu110 -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
 ```
 
 ## Run
--- setup.sh
+++ setup.sh
@@ -1,21 +1,22 @@
-# conda environment
 conda create -n roca python=3.8 -y
 conda activate roca
 
-# Simple deps
-pip install \
-  dataclasses opencv-python numpy-quaternion pandas scipy trimesh rtree numba
+cd ..
+git clone --recursive https://github.com/cangumeli/Scan2CADRasterizer.git
+cd Scan2CADRasterizer
+pip install .
+cd ../image-to-cad
 
-pip install open3d==0.13.0
+pip install dataclasses opencv-python numpy-quaternion \
+  pandas scipy trimesh rtree numba open3d==0.13.0
 
-pip install fvcore iopath
-# pytorch3d
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
 conda install -c bottler nvidiacub -y
-# NOTE: Tested with 0.5 and 0.6
 conda install pytorch3d -c pytorch3d-nightly -y
 
-pip install torch==1.7.0+cu110 torchvision==0.8.1+cu110 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.7.0+cu110 torchvision==0.8.1+cu110 torchaudio==0.7.0 -f \
+  https://download.pytorch.org/whl/torch_stable.html
 
-# detectron2
 python -m pip install detectron2==0.3+cu110 -f \
   https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
+
