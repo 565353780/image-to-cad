6be0b218abf015f6e7715bbcc8a6b2c8babc6322
565353780 <565353780@qq.com>
2022-04-19 22:06:04 +0800

    [RUN] update available pkgs' version

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ conda create -n roca python=3.8 -y
 conda activate roca
 
 # Simple deps
-pip install -r requirements.txt
+pip install dataclasses opencv-python numpy-quaternion pandas scipy trimesh rtree numba
+
+pip install open3d==0.13.0
 
 # pytorch3d
 conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
@@ -11,12 +13,8 @@ conda install -c bottler nvidiacub -y
 # NOTE: Tested with 0.5 and 0.6
 conda install pytorch3d -c pytorch3d-nightly -y
 
-# pytorch
 pip install torch==1.7.0+cu110 torchvision==0.8.1+cu110 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 # detectron2
 python -m pip install detectron2==0.3+cu110 -f \
   https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
-
-# numba
-pip install numba
