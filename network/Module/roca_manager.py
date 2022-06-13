6a58f717f473f7a05c1af96f1f72220fff41e2b4
chLi <565353780@qq.com>
2022-06-13 17:46:24 +0800

    remove unused code

--- network/Module/roca_manager.py
+++ network/Module/roca_manager.py
@@ -13,9 +13,6 @@ from habitat_sim_manage.Data.pose import Pose
 from Module.roca_sim_detector import ROCASimDetector
 from Module.roca_merger import ROCAMerger
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 class ROCAManager(object):
     def __init__(self):
         self.roca_sim_detector = ROCASimDetector()
@@ -25,20 +22,6 @@ class ROCAManager(object):
     def reset(self):
         self.roca_sim_detector.reset()
         self.roca_merger.reset()
-        return True
-
-    def showPoints(self, point_list):
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-
-        for i in range(int(len(point_list) / 2)):
-            start = point_list[2 * i]
-            end = point_list[2 * i + 1]
-            ax.quiver(start[0], start[1], start[2],
-                  end[0]-start[0], end[1]-start[1], end[2]-start[2],
-                  arrow_length_ratio=0.1)
-
-        plt.show()
         return True
 
     def loadSettings(self, roca_settings, sim_settings=None):
