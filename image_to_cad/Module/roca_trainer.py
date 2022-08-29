3228c507927d0176ca673551f231aba692925971
chLi <565353780@qq.com>
2022-08-29 18:15:37 +0800

    finish trainer

--- image_to_cad/Module/roca_trainer.py
+++ image_to_cad/Module/roca_trainer.py
@@ -14,7 +14,7 @@ from network.roca.data import CategoryCatalog
 from network.roca.data.datasets import register_scan2cad
 from network.roca.engine import Trainer
 
-cfg = {
+TRAIN_CONFIG = {
     "data_dir": "./Dataset/Dataset/",
     "image_root": "./Dataset/Images/",
     "rendering_root": "./Dataset/Rendering/",
@@ -30,7 +30,7 @@ cfg = {
     "eval_period": 2500,
     "freq_scale": "image", # ['none', 'image', 'cad]
 
-    "steps": 60000,
+    "steps": [60000],
     "workers": 0,
     "eval_step": 0,
     "gclip": 10.0,
@@ -89,70 +89,71 @@ def register_data(config):
 
     return train_name, val_name
 
-def make_config(train_name, val_name, args):
+def make_config(config):
+    train_name, val_name = register_data(config)
+
     cfg = roca_config(
         train_data=train_name,
         test_data=val_name,
-        batch_size=args.batch_size,
-        num_proposals=args.num_proposals,
+        batch_size=config["batch_size"],
+        num_proposals=config["num_proposals"],
         num_classes=CategoryCatalog.get(train_name).num_classes,
-        max_iter=args.max_iter,
-        lr=args.lr,
-        output_dir=args.output_dir,
-        eval_period=args.eval_period,
-        eval_step=bool(args.eval_step),
-        workers=args.workers,
+        max_iter=config["max_iter"],
+        lr=config["lr"],
+        output_dir=config["output_dir"],
+        eval_period=config["eval_period"],
+        eval_step=bool(config["eval_step"]),
+        workers=config["workers"],
         class_freqs=CategoryCatalog.get(train_name).freqs,
-        steps=args.steps,
-        pooler_size=args.pooler_size,
-        batch_average=bool(args.batch_average),
-        depth_grad_losses=bool(args.depth_grad_losses),
-        per_category_mask=bool(args.per_category_mask),
-        per_category_noc=bool(args.per_category_noc),
-        noc_embed=bool(args.noc_embed),
-        noc_weights=bool(args.noc_weights),
-        per_category_trans=bool(args.per_category_trans),
-        noc_weight_head=bool(args.custom_noc_weights),
-        noc_weight_skip=bool(args.noc_weight_skip),
-        noc_rot_init=bool(args.noc_rot_init),
-        seed=args.seed,
-        gclip=args.gclip,
-        augment=bool(args.augment),
-        zero_center=bool(args.zero_center),
-        irls_iters=args.irls_iters,
-        retrieval_mode=args.retrieval_mode,
-        wild_retrieval=bool(args.wild_retrieval),
-        confidence_thresh_test=args.confidence_thresh_test,
-        e2e=bool(args.e2e)
+        steps=config["steps"],
+        pooler_size=config["pooler_size"],
+        batch_average=bool(config["batch_average"]),
+        depth_grad_losses=bool(config["depth_grad_losses"]),
+        per_category_mask=bool(config["per_category_mask"]),
+        per_category_noc=bool(config["per_category_noc"]),
+        noc_embed=bool(config["noc_embed"]),
+        noc_weights=bool(config["noc_weights"]),
+        per_category_trans=bool(config["per_category_trans"]),
+        noc_weight_head=bool(config["custom_noc_weights"]),
+        noc_weight_skip=bool(config["noc_weight_skip"]),
+        noc_rot_init=bool(config["noc_rot_init"]),
+        seed=config["seed"],
+        gclip=config["gclip"],
+        augment=bool(config["augment"]),
+        zero_center=bool(config["zero_center"]),
+        irls_iters=config["irls_iters"],
+        retrieval_mode=config["retrieval_mode"],
+        wild_retrieval=bool(config["wild_retrieval"]),
+        confidence_thresh_test=config["confidence_thresh_test"],
+        e2e=bool(config["e2e"])
     )
 
     # NOTE: Training state will be reset in this case!
-    if args.checkpoint.lower() not in ('', 'none'):
-        cfg.MODEL.WEIGHTS = args.checkpoint
+    if config["checkpoint"].lower() not in ('', 'none'):
+        cfg.MODEL.WEIGHTS = config["checkpoint"]
 
     return cfg
 
-def setup_output_dir(args, cfg):
-    output_dir = args.output_dir
-    assert not args.resume or path.isdir(args.output_dir), \
-        'No backup found in {}'.format(args.output_dir)
-    makedirs(output_dir, exist_ok=args.override_output or args.resume)
+def setup_output_dir(config, cfg):
+    output_dir = config["output_dir"]
+    assert not config["resume"] or path.isdir(config["output_dir"]), \
+        'No backup found in {}'.format(config["output_dir"])
+    makedirs(output_dir, exist_ok=config["override_output"] or config["resume"])
 
-    if not args.eval_only and not args.resume:
+    if not config["eval_only"] and not config["resume"]:
         # Save command line arguments
-        with open(path.join(args.output_dir, 'args.json'), 'w') as f:
-            json.dump(vars(args), f)
+        with open(path.join(config["output_dir"], 'args.json'), 'w') as f:
+            json.dump(config, f)
         # Save the config as yaml
         with open(path.join(output_dir, 'config.yaml'), 'w') as f:
             cfg.dump(stream=f)
 
-
-def train_or_eval(args, cfg):
+def train_or_eval(config, cfg):
     trainer = Trainer(cfg)
-    trainer.resume_or_load(resume=args.resume)
-    if args.eval_only:
+    trainer.resume_or_load(resume=config["resume"])
+    if config["eval_only"]:
         trainer.test(cfg, trainer.model)
-    elif args.resume:
+    elif config["resume"]:
         trainer.test(cfg, trainer.model)
         trainer.train()
     else:
@@ -163,9 +164,8 @@ class ROCATrainer(object):
         return
 
 def demo():
-    train_name, val_name = register_data(cfg)
-    cfgs = make_config(train_name, val_name, cfg)
-    setup_output_dir(cfg, cfgs)
-    train_or_eval(cfg, cfgs)
+    cfg = make_config(TRAIN_CONFIG)
+    setup_output_dir(TRAIN_CONFIG, cfg)
+    train_or_eval(TRAIN_CONFIG, cfg)
     return True
 
