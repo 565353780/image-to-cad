94bf18346c2f2cdb28cf7d9d8dbea2d3b5d81a1e
chLi <565353780@qq.com>
2022-08-29 14:56:41 +0800

    add roca_detect_demo

--- roca_demo.py
+++ roca_demo.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from image_to_cad.Module.roca_manager import demo
+from image_to_cad.Module.roca_manager import demo as roca_sim_demo
+from image_to_cad.Module.roca_detector import demo as roca_detect_demo
 
 if __name__ == "__main__":
-    demo()
+    #  roca_sim_demo()
+    roca_detect_demo()
 
