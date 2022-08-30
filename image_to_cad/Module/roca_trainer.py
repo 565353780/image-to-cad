29c977390b879bcfcc89dec91e1dd35b1a95b48e
chLi <565353780@qq.com>
2022-08-30 09:57:39 +0800

    finish basic trainer class

--- image_to_cad/Module/roca_trainer.py
+++ image_to_cad/Module/roca_trainer.py
@@ -5,7 +5,6 @@ import sys
 sys.path.append("./network/")
 
 import json
-import argparse
 import os.path as path
 from os import makedirs
 
@@ -14,52 +13,7 @@ from network.roca.data import CategoryCatalog
 from network.roca.data.datasets import register_scan2cad
 from network.roca.engine import Trainer
 
-TRAIN_CONFIG = {
-    "data_dir": "./Dataset/Dataset/",
-    "image_root": "./Dataset/Images/",
-    "rendering_root": "./Dataset/Rendering/",
-    "full_annot": "./Dataset/full_annotations.json",
-
-    "output_dir": "./output/",
-
-    "lr": 1e-3,
-    "max_iter": 80000,
-    "batch_size": 4,
-    "num_proposals": 128,
-    "eval_period": 2500,
-    "freq_scale": "image", # ['none', 'image', 'cad]
-
-    "steps": [60000],
-    "workers": 0,
-    "eval_step": 0,
-    "gclip": 10.0,
-    "augment": 1,
-
-    "pooler_size": 16,
-    "batch_average": 0,
-    "depth_grad_losses": 0,
-    "per_category_mask": 1,
-    "enable_nocs": 1,
-    "per_category_noc": 0,
-    "noc_embed": 0,
-    "noc_weights": 1,
-    "per_category_trans": 1,
-    "custom_noc_weights": 1,
-    "noc_weight_skip": 0,
-    "noc_rot_init": 0,
-    "zero_center": 0,
-    "irls_iters": 1,
-    "retrieval_mode": "resnet_resnet+image+comp",
-    "wild_retrieval": 0,
-    "confidence_thresh_test": 0.5,
-    "e2e": 1,
-
-    "eval_only": 0,
-    "checkpoint": "./output/last_checkpoint",
-    "resume": 1,
-
-    "seed": 2021,
-}
+from image_to_cad.Config.config import TRAIN_CONFIG
 
 def register_data(config):
     data_dir = config["data_dir"]
@@ -137,32 +91,36 @@ def setup_output_dir(config, cfg):
     output_dir = config["output_dir"]
     makedirs(output_dir, exist_ok=True)
 
-    if not config["eval_only"] and not config["resume"]:
-        # Save command line arguments
-        with open(path.join(config["output_dir"], 'args.json'), 'w') as f:
-            json.dump(config, f)
-        # Save the config as yaml
-        with open(path.join(output_dir, 'config.yaml'), 'w') as f:
-            cfg.dump(stream=f)
-
-def train_or_eval(config, cfg):
-    trainer = Trainer(cfg)
-    trainer.resume_or_load(resume=config["resume"])
-    if config["eval_only"]:
-        trainer.test(cfg, trainer.model)
-    elif config["resume"]:
-        trainer.test(cfg, trainer.model)
-        trainer.train()
-    else:
-        trainer.train()
-
-class ROCATrainer(object):
-    def __init__(self):
+    if config["resume"]:
         return
 
+    # Save command line arguments
+    with open(path.join(config["output_dir"], 'args.json'), 'w') as f:
+        json.dump(config, f)
+    # Save the config as yaml
+    with open(path.join(output_dir, 'config.yaml'), 'w') as f:
+        cfg.dump(stream=f)
+    return
+
+class ROCATrainer(object):
+    def __init__(self, config):
+        self.cfg = make_config(TRAIN_CONFIG)
+        setup_output_dir(TRAIN_CONFIG, self.cfg)
+        self.trainer = Trainer(self.cfg)
+        self.trainer.resume_or_load(resume=config["resume"])
+        return
+
+    def train(self):
+        self.trainer.train()
+        return True
+
+    def test(self):
+        self.trainer.test(self.cfg, self.trainer.model)
+        return True
+
 def demo():
-    cfg = make_config(TRAIN_CONFIG)
-    setup_output_dir(TRAIN_CONFIG, cfg)
-    train_or_eval(TRAIN_CONFIG, cfg)
+    roca_trainer = ROCATrainer(TRAIN_CONFIG)
+
+    roca_trainer.train()
     return True
 
