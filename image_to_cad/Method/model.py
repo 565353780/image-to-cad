e65b96901c65269a3f6a6f54cb2a0480cc9771d6
chLi <565353780@qq.com>
2022-09-19 23:40:20 +0800

    finish change model

--- image_to_cad/Method/model.py
+++ image_to_cad/Method/model.py
@@ -11,9 +11,9 @@ def changeModelName(model_path, new_model_path):
 
     for key, item in backup['model'].items():
         new_name = key
-        if "roi_heads.alignment_head.retrieval_head" in key:
-            new_name = key.replace("roi_heads.alignment_head.retrieval_head",
-                                   "roi_heads.retrieval_head")
+        if "roi_heads" in key:
+            new_name = key.replace("roi_heads",
+                                   "roi_head")
         new_state_dict[new_name] = item
         backup['model'] = new_state_dict
 
