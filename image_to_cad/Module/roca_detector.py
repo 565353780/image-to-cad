7b57b18f21e2fe19f1b016c2c89eaacdcf7e3087
chLi <565353780@qq.com>
2022-09-20 14:20:22 +0800

    update render function, detect test passed

--- image_to_cad/Module/roca_detector.py
+++ image_to_cad/Module/roca_detector.py
@@ -193,7 +193,7 @@ def demo():
         scene_name = scene_name_dict[name]
         roca_detector.detectImageFromPath('./network/assets/' + name + '.jpg', scene_name)
         result = roca_detector.getResultDict()
-        #  roca_detector.renderResultImage()
+        #  roca_detector.renderResultWithProcess()
     return True
 
 if __name__ == '__main__':
