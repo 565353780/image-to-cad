b25ab7a47e6f543f29b0966ae9169bb808a2e187
chLi <565353780@qq.com>
2022-09-20 17:08:38 +0800

    finish remove pixel_mean and pixel_std values in models

--- image_to_cad/Method/model.py
+++ image_to_cad/Method/model.py
@@ -11,11 +11,10 @@ def changeModelName(model_path, new_model_path):
 
     for key, item in backup['model'].items():
         new_name = key
-        if "roi_heads" in key:
-            new_name = key.replace("roi_heads",
-                                   "roi_head")
+        if key in ["pixel_mean", "pixel_std"]:
+            continue
         new_state_dict[new_name] = item
-        backup['model'] = new_state_dict
+    backup['model'] = new_state_dict
 
     torch.save(backup, new_model_path)
     return True
